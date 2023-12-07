39da486c8c220c4851a72ab883178c4c57598008
pablopissoni <pablopissoni@gmail.com>
2023-12-07 18:23:03 -0300

    se modifico color y texto de footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="footer-distributed">
+      <div className="footer-left">
+        <h3>
+          Butacas <span>Equitanti</span>
+        </h3>
 
-			<div className="footer-left">
-
-				<h3>Company<span>logo</span></h3>
-
-				<p className="footer-links">
-					<a href="#" className="link-1">Home</a>
+        {/* <p className="footer-links">
+					<a href="#" className="link-1"
+					onClick={() => handleLinkClick("trabajos-anteriores")}
+					>Inicio</a>
 					
 					<a href="#">Blog</a>
 				
@@ -21,48 +23,55 @@ export const Footer = () => {
 					<a href="#">Faq</a>
 					
 					<a href="#">Contact</a>
-				</p>
+				</p> */}
 
-				<p className="footer-company-name">Company Name © 2015</p>
-			</div>
+        {/* <p className="footer-company-name">Company Name © 2015</p> */}
+      </div>
 
-			<div className="footer-center">
+      <div className="footer-center">
+        <div>
+          <i className="fa fa-map-marker"></i>
+          <p>
+            <span>444 S. Cedros Ave</span> Solana Beach, California
+          </p>
+        </div>
 
-				<div>
-					<i className="fa fa-map-marker"></i>
-					<p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
-				</div>
-
-				<div>
+        {/* <div>
 					<i className="fa fa-phone"></i>
 					<p>+1.555.555.5555</p>
-				</div>
+				</div> */}
 
-				<div>
-					<i className="fa fa-envelope"></i>
-					<p><a href="mailto:support@company.com">support@company.com</a></p>
-				</div>
+        <div>
+          <i className="fa fa-envelope"></i>
+          <p>
+            <a href="mailto:support@company.com">
+              butacasequitanti@hotmail.com
+            </a>
+          </p>
+        </div>
+      </div>
 
-			</div>
+      <div className="footer-right">
+        <p className="footer-company-about">
+          <span>Nuestra compania</span>
+          EQUITANTI, desde 1942, líder en equipamiento para cines y teatros.
+          Tradición, calidad, innovación. Preferidos por las principales salas
+          del país.
+        </p>
 
-			<div className="footer-right">
-
-				<p className="footer-company-about">
-					<span>About the company</span>
-					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-				</p>
-
-				<div className="footer-icons">
+        {/* <div className="footer-icons">
 
 					<a href="#"><i className="fa fa-facebook"></i></a>
 					<a href="#"><i className="fa fa-twitter"></i></a>
 					<a href="#"><i className="fa fa-linkedin"></i></a>
 					<a href="#"><i className="fa fa-github"></i></a>
 
-				</div>
+				</div> */}
+      </div>
 
-			</div>
-
-		</footer>
-  )
-}
+      <p className="copyright">
+        Butacas Equitanti &copy Todos los derechos reservados - 2024
+      </p>
+    </footer>
+  );
+};
